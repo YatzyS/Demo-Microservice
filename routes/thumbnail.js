00a751b831a8cbcf5a1c90e7058319849b9b4c5b
Yash <yasket@gmail.com>
2020-05-18 19:30:49 +0530

    Authentication done

--- routes/thumbnail.js
+++ routes/thumbnail.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var wt = require("../utilities/webtokens.js")
 
 router.get('/', function(req, res) {
-  res.send('thumbnail');
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if(token == null) return res.status(401).send("No token found in request")
+
+  wt.authenticateToken(token, (err, user) => {
+    if(err != null) return res.status(403).send("Invalid token")
+    res.send(user)
+  })
 });
 
 module.exports = router;
