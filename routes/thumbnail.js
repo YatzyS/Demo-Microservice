953b7fe61444ddc03f21a61a7939a1a6f71207a1
Yash <yasket@gmail.com>
2020-05-18 20:24:47 +0530

    Thumbnail conversion working

--- routes/thumbnail.js
+++ routes/thumbnail.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var jimp = require('jimp')
+var bodyParser = require('body-parser')
 var wt = require("../utilities/webtokens.js")
+
+router.use(bodyParser.json())
 
 router.get('/', function(req, res) {
   const authHeader = req.headers['authorization']
@@ -9,7 +13,14 @@ router.get('/', function(req, res) {
 
   wt.authenticateToken(token, (err, user) => {
     if(err != null) return res.status(403).send("Invalid token")
-    res.send(user)
+      const imgUrl = req.body.url
+      jimp.read(imgUrl, (err, image) => {
+        if(err != null) return res.status(404).send("Image not found") 
+        image.resize(50,50).getBase64(jimp.AUTO, (err, uri)=> {
+          if(err != null) return res.status(404).send("Image not found")
+          res.send(uri)
+        })
+      })
   })
 });
 
