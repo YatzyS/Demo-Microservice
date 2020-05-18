c30ecb06a568ab0ed01aed75220c74c062e04bbd
Yash <yasket@gmail.com>
2020-05-18 18:11:51 +0530

    Getting JWT in response

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,23 @@
+require('dotenv').config()
+
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser')
 
-router.get('/', function(req, res) {
-  res.send('login');
-});
+const jwt = require("jsonwebtoken")
 
-module.exports = router;
+router.use(bodyParser.json())
+
+router.post('/', function(req, res) {
+  const username = req.body.username 
+  const password = req.body.password 
+  //Authenticate user
+  if(username != undefined && password != undefined) {
+    const user = { uname: username, pass: password }
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToke: accessToken })
+  }
+  res.status(404).send("Invalid Input")
+}) 
+
+module.exports = router 
